62eece1cdff25baa3bbdbfca0d355d509fa6b314
suhibabbas <sohaib_salah96@yahoo.com>
2022-05-29 01:36:55 +0300

    repeated word

--- hashTable/app/src/main/java/hashTable/App.java
+++ hashTable/app/src/main/java/hashTable/App.java
@@ -23,9 +23,9 @@ public class App {
 
         System.out.println(hashMap.get("Bob"));
         System.out.println(hashMap.getSize());
-        
+
         System.out.println(repeatedWord("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."));
-        System.out.println(repeatedWord("Once upon a time, there was a brave princess who..."));
+        System.out.println(repeatedWord(""));
 
 
     }
